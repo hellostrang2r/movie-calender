4f5dddedcf081fb3e746278c300442b99a770002
hellostrang2r <bosko413@gmail.com>
2026-04-12 00:47:37 +0900

    update web

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "f207c767c39225e6d4dfaec50b469a09",
+const RESOURCES = {"flutter_bootstrap.js": "d922d72c9b1a67d6bbe2fd0682cc3219",
 "version.json": "ddc1339d665a41fc26bf47a9bd1a5006",
 "index.html": "5aebc542c0643193710e5dc2405d081c",
 "/": "5aebc542c0643193710e5dc2405d081c",
-"main.dart.js": "01847fc6d3e4451976a4835ca0776481",
+"main.dart.js": "ebd20030017366e9b934f3793b3a6250",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
